fd6d49652ca6beaca17296f931d10682ae5b364a
tannerkrewson <tannerkrewson@gmail.com>
2017-06-20 11:03:54 -0400

    Authorize with Spotify

--- js/main.js
+++ js/main.js
@@ -1,8 +1,19 @@
+const SPOTIFY_CLIENT_ID = '3c982a456c594c39b23403937c5d2343';
+
 function main () {
-  showPlaylistAdder();
-  playlistList = new PlaylistList ('#playlist-list');
-  playlistList.show();
-  setupPlaylistAdding(playlistList);
+  var spotifyHash = checkForSpotifyAccessToken();
+
+  if (spotifyHash) {
+    showPlaylistAdder();
+    playlistList = new PlaylistList ('#playlist-list');
+    playlistList.show();
+    setupPlaylistAdding(playlistList);
+
+    var spotifyApi = new SpotifyWebApi();
+
+  } else {
+    showSpotifyLoginButton();
+  }
 }
 
 function showPlaylistAdder () {
@@ -28,6 +39,50 @@ function setupPlaylistAdding (playlistList) {
     playlistList.add(playlistToAdd);
     playlistList.render();
   });
+}
+
+function showSpotifyLoginButton () {
+  var loginBtn = $('#spotify-login-btn');
+  loginBtn.click(loginToSpotify);
+  loginBtn.show();
+}
+
+function loginToSpotify () {
+  location.href = "https://accounts.spotify.com/authorize" +
+    "?client_id=" + SPOTIFY_CLIENT_ID +
+    "&response_type=token" +
+    "&redirect_uri=" + encodeURIComponent(window.location.href) +
+    //"&state=" + STATE + // optional
+    //"&scope=" + SCOPES.join(" ") + // optional
+    "";
+}
+
+function checkForSpotifyAccessToken () {
+  // https://stackoverflow.com/questions/38706233/javascript-mime-type-text-html-is-not-executable-and-strict-mime-type-chec
+
+  var hash = {};
+  var h = location.hash.slice(1);
+
+  if (h !== '') {
+    h = h.split('&');
+    h.forEach(function(pair) {
+        pair = pair.split('=');
+        hash[pair.shift()] = pair.join('=');
+    });
+
+    if (hash.error) {
+        console.log(hash.error);
+        return false;
+    } else {
+        hash.token_type === "Bearer";
+        //remove the junk from the url
+        history.pushState('', document.title, window.location.pathname);
+        return hash;
+    }
+  } else {
+    return false;
+  }
+
 }
 
 function getURLFromInput () {
